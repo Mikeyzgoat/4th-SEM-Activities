841effec54b8fcc93aa2e015d9384f3d8d73ace8
Kibutsuji <tadisai091@gmail.com>
2022-08-30 08:43:15 +0530

    Handling repeated words

--- Activities/OOC ACTIVITY/pos.java
+++ Activities/OOC ACTIVITY/pos.java
@@ -3,13 +3,14 @@ import org.json.JSONObject;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.LinkedHashSet;
 import java.util.Scanner;
 public class pos {
-	public void thesaurus(String st){
+	public static void thesaurus(String st){
 		try{
 			HttpRequest request = HttpRequest.newBuilder()
 			.uri(URI.create("https://synonyms-word-info.p.rapidapi.com/v1/word?str="+st))
-			.header("X-RapidAPI-Key", "47b56b3dd5mshf5dbd0345c52e8cp1615fdjsncfbf112e88e4")
+			.header("X-RapidAPI-Key","47b56b3dd5mshf5dbd0345c52e8cp1615fdjsncfbf112e88e4")
 			.header("X-RapidAPI-Host", "synonyms-word-info.p.rapidapi.com")
 			.method("GET", HttpRequest.BodyPublishers.noBody())
 			.build();
@@ -19,9 +20,6 @@ public class pos {
         print(st+" : ");
         for(int i=1;i<=5;i++){
             String str = (Jobj).getString("pos"+ i);
-            if(str == "etc" && (st == "a" || st == "an")){
-                str = "det";
-            }
             if(str != "")
                 System.out.print(str+" ");
         }
@@ -39,22 +37,29 @@ public class pos {
         System.out.print(line);
     }
 
-
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        pos obj = new pos();
-        print("Enter String : ");
-        String st = sc.nextLine();
-        st = st.replaceAll("[,./'?!@#$%^&8()-+=`~_{}|<>0123456789]", " ");
-        String str[] = st.split(" ");
-        for(String i :str){
-            if(i == " " || i == ""){
+    public static void dupes(String str[]){
+        LinkedHashSet<String> set = new LinkedHashSet<String>();
+        for(String i : str){set.add(i);}
+        for(String s : set){
+            if(s == "" || s == " "){
                 continue;
             }
             else{
-                obj.thesaurus(i);
+                thesaurus(s);
             }
+            
         }
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        print("Enter String : ");
+        String st = sc.nextLine();
+        st = st.replaceAll("[,./'?!@#$%^&*()-+=`~_{}|<>0123456789]", " ");
+        String str[] = st.split(" ");
+
+        dupes(str); // used to remove redundant strings
+        
         sc.close();
     }
 }
